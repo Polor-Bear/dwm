12af32cc7c9c4e29e492b64455510c48029b1ee3
Polor-Bear <1097041650@qq.com>
2022-04-16 02:43:23 +0800

    change color

--- config.h
+++ config.h
@@ -19,8 +19,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "cascadia code:size=16" };
 static const char dmenufont[]       = "cascadia code:size=16";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#333311";
+static const char col_gray1[]       = "#26363E";
+static const char col_gray2[]       = "#26363E";
 static const char col_gray3[]       = "#aa00ff";
 static const char col_gray4[]       = "#00ffff";
 static const char col_cyan[]        = "#37474F";
